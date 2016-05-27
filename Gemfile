75aa6c8455b6f80cc7e7b37f1c85da9772e7c61e
L.Joel <leopard10000@gmail.com>
2016-05-27 22:17:20 +0900

    SG RoR.4

--- Gemfile
+++ Gemfile
@@ -40,6 +40,10 @@ end
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
+
+  # Rails Console 上で ActiveRecord の結果を見やすく整形してくれるツール
+  gem 'hirb'
+  gem 'hirb-unicode'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,35 @@
 class Entry < ActiveRecord::Base
+  has_many :comments
+
+
+  # 親子関係を持つデータの登録方法 【rails console使用】
+  # ※http://qiita.com/jnchito/items/7f41ff3df900909952db
+
+  # 1.parent.children.create/build ※buildはsaveしないので別途saveが必要
+  # e = Entry.create(title: "title1", body: "b1\r\nb2")
+  # c = e.comments.create(body: "nice!", status: "approved")
+
+  # 2.parent.children << child
+  # e = Entry.find_by_id(1)
+  # c = Comment.new(body: "b1", status: "unapproved")
+  # e.comments << c  ※この時点でsaveされる
+
+  # ※parent.children = children
+  #   = で配列を渡すと、既存のchildrenは全て削除される。
+
+  # 3.child.parent = parent
+  # c.entry = e
+  # c.save
+
+  # createと同時に親もセットするパターン
+  # c = Comment.create(body: "b", status: "approved", entry: e)
+
+  # 4.child.parent_id = parent_id
+  # c.entry_id = 2
+  # c.save
+  # Entry.find(2).size  ※確認
+
+  # createと同時に親idもセットするパターン
+  # c = Comment.create(body: "b", status: "approved", entry_id: e.id)
+
 end
